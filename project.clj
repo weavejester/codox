f3b5d7421186acb2e94c8ef94a2f4b4f92f54736
James Reeves <jreeves@weavejester.com>
2011-10-30 01:14:26 +0100

    Initial commit

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject codox "0.0.1"
+  :description "Generate documentation from Clojure source files"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/tools.namespace "0.1.0"]])
---
+++ test/codox/test/core.clj
@@ -0,0 +1,6 @@
+(ns codox.test.core
+  (:use [codox.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
