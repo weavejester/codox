f639b8b368999c793381136b00314b7bc15ec6ea
James Reeves <jreeves@weavejester.com>
2011-10-30 16:11:23 +0000

    Writer generates namespace pages

--- src/codox/writer/html.clj
+++ src/codox/writer/html.clj
@@ -1,18 +1,44 @@
 (ns codox.writer.html
   "Documentation writer that outputs HTML."
-  (:use [hiccup core page-helpers]))
+  (:use [hiccup core page-helpers])
+  (:require [clojure.java.io :as io]))
+
+(defn- ns-filename [namespace]
+  (str (:name namespace) ".html"))
+
+(defn- ns-filepath [namespace]
+  (str "doc/" (ns-filename namespace)))
 
 (defn- make-index
   [namespaces]
   (html5
    [:head
-    [:title "Documentation"]]
+    [:title "API documentation"]]
    [:body
+    [:h1 "API documentation"]
     (for [namespace namespaces]
       [:div.namespace
-       [:h1 (:name namespace)]])]))
+       [:h2 (link-to (ns-filename namespace) (:name namespace))]
+       [:pre.doc (:doc namespace)]])]))
+
+(defn- make-ns-page
+  [namespace]
+  (html5
+   [:head
+    [:title (:name namespace) " documentation"]]
+   [:body
+    [:h1 (:name namespace) " documentation"]
+    [:div.doc (:doc namespace)]
+    (for [public (:publics namespace)]
+      [:div.public
+       [:h3 (:name public)]
+       [:pre (pr-str (:arglists public))]
+       [:pre.doc "  " (:doc public)]])]))
 
 (defn write-docs
   "Take raw documentation info and turn it into formatted HTML."
   [info]
-  (spit "index.html" (make-index info)))
+  (.mkdirs (io/file "doc"))
+  (spit "doc/index.html" (make-index info))
+  (doseq [namespace info]
+    (spit (ns-filepath namespace) (make-ns-page namespace))))
