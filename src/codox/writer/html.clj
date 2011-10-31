6841f944a2092f3e2c83313a95e36ed3c9ad0f42
James Reeves <jreeves@weavejester.com>
2011-10-31 03:47:10 +0000

    Added index links to each namespace page

--- src/codox/writer/html.clj
+++ src/codox/writer/html.clj
@@ -12,47 +12,45 @@
 (defn- var-uri [namespace var]
   (str (ns-filename namespace) "#" (:name var)))
 
-(defn html-doc [title & body]
+(defn- html-page [title & body]
   (html5
    [:head
     [:title title]
     (include-css "css/default.css")]
-   [:body body]))
+   [:body
+    [:h1 title]
+    body]))
+
+(defn- make-var-links [namespace]
+  [:ul.index
+   (for [var (:publics namespace)]
+     [:li (link-to (var-uri namespace var) (:name var))])])
 
 (defn- make-index [namespaces]
-  (html5
-   [:head
-    [:title "API documentation"]
-    (include-css "css/default.css")]
-   [:body
-    [:h1 "API documentation"]
-    (for [namespace namespaces]
-      [:div.namespace
-       [:h2 (link-to (ns-filename namespace) (:name namespace))]
-       [:pre.doc (:doc namespace)]
-       [:ul.publics
-        (for [var (:publics namespace)]
-          [:li (link-to (var-uri namespace var) (:name var))])]])]))
+  (html-page
+   "API documentation"
+   (for [namespace namespaces]
+     [:div.namespace
+      [:h2 (link-to (ns-filename namespace) (:name namespace))]
+      [:pre.doc (:doc namespace)]
+      (make-var-links namespace)])))
 
 (defn- var-usage [var]
   (for [arglist (:arglists var)]
     (list* (:name var) arglist)))
 
 (defn- make-ns-page [namespace]
-  (html5
-   [:head
-    [:title (:name namespace) " documentation"]
-    (include-css "css/default.css")]
-   [:body
-    [:h1 (:name namespace) " documentation"]
-    [:pre.doc (:doc namespace)]
-    (for [var (:publics namespace)]
+  (html-page
+   (str (:name namespace) " documentation")
+   [:pre.doc (:doc namespace)]
+   (make-var-links namespace)
+   (for [var (:publics namespace)]
       [:div.public {:id (:name var)}
        [:h3 (:name var)]
        [:div.usage
         (for [form (var-usage var)]
           [:code (pr-str form)])]
-       [:pre.doc (:doc var)]])]))
+       [:pre.doc (:doc var)]])))
 
 (defn- copy-resource [src dest]
   (io/copy (io/input-stream (io/resource src))
