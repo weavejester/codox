d65e72f22719cbd6572e3b8236bd81650d629771
James Reeves <jreeves@weavejester.com>
2011-11-06 13:46:26 +0000

    Released 0.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject codox "0.2.0"
+(defproject codox "0.2.1"
   :description "Generate documentation from Clojure source files"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/tools.namespace "0.1.0"]
