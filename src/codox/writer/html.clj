319557e53e332efbc9a3ae1f8ed111cd550ca3fd
James Reeves <jreeves@weavejester.com>
2011-11-06 23:02:25 +0000

    Ensure all user information is escaped, and ids don't conflict

--- src/codox/writer/html.clj
+++ src/codox/writer/html.clj
@@ -10,14 +10,17 @@
 (defn- ns-filepath [namespace]
   (str "doc/" (ns-filename namespace)))
 
+(defn- var-id [var]
+  (str "var-" (:name var)))
+
 (defn- var-uri [namespace var]
-  (str (ns-filename namespace) "#" (:name var)))
+  (str (ns-filename namespace) "#" (var-id var)))
 
 (defn- link-to-ns [namespace]
-  (link-to (ns-filename namespace) (:name namespace)))
+  (link-to (ns-filename namespace) (h (:name namespace))))
 
 (defn- link-to-var [namespace var]
-  (link-to (var-uri namespace var) (:name var)))
+  (link-to (var-uri namespace var) (h (:name var))))
 
 (defn- namespaces-menu [project & [namespace]]
   [:div#namespaces.sidebar
@@ -50,23 +53,23 @@
 
 (defn- header [project]
   [:div#header
-   [:h1 (link-to "index.html" (project-title project))]])
+   [:h1 (link-to "index.html" (h (project-title project)))]])
 
 (defn- index-page [project]
   (html5
    [:head
     default-includes
-    [:title (project-title project)]]
+    [:title (h (project-title project))]]
    [:body
     (header project)
     (namespaces-menu project)
     [:div#content.namespace-index
-     [:h2 (project-title project)]
-     [:div.doc (:description project)]
+     [:h2 (h (project-title project))]
+     [:div.doc (h (:description project))]
      (for [namespace (:namespaces project)]
        [:div.namespace
         [:h3 (link-to-ns namespace)]
-        [:pre.doc (:doc namespace)]
+        [:pre.doc (h (:doc namespace))]
         [:div.index
          [:p "Public variables and functions:"]
          (var-links namespace)]])]]))
@@ -82,21 +85,21 @@
   (html5
    [:head
     default-includes
-    [:title (namespace-title namespace)]]
+    [:title (h (namespace-title namespace))]]
    [:body
     (header project)
     (namespaces-menu project namespace)
     (vars-menu namespace)
     [:div#content.namespace-docs
-     [:h2 (namespace-title namespace)]
-     [:pre.doc (:doc namespace)]
+     [:h2 (h (namespace-title namespace))]
+     [:pre.doc (h (:doc namespace))]
      (for [var (:publics namespace)]
-      [:div.public {:id (:name var)}
-       [:h3 (:name var)]
-       [:div.usage
-        (for [form (var-usage var)]
-          [:code (pr-str form)])]
-       [:pre.doc (:doc var)]])]]))
+       [:div.public {:id (h (var-id var))}
+        [:h3 (h (:name var))]
+        [:div.usage
+         (for [form (var-usage var)]
+           [:code (h (pr-str form))])]
+        [:pre.doc (h (:doc var))]])]]))
 
 (defn- copy-resource [src dest]
   (io/copy (io/input-stream (io/resource src))
