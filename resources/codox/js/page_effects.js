ae4e0ca3ac7ab8219e1c4f11f62feb70b5958e8c
James Reeves <jreeves@weavejester.com>
2011-11-30 01:00:32 +0000

    Namespace scroll position saved when switching page

--- resources/codox/js/page_effects.js
+++ resources/codox/js/page_effects.js
@@ -22,5 +22,30 @@ function setCurrentVarLink() {
     })
 }
 
-$(window).load(setCurrentVarLink)
-$(window).load(function() { $('#content').scroll(setCurrentVarLink) })
+var hasStorage = (function() { try { return localStorage.getItem } catch(e) {} }())
+
+function scrollPositionId(element) {
+  var directory = window.location.href.replace(/[^\/]+\.html$/, '')
+  return 'scroll::' + $(element).attr('id') + '::' + directory
+}
+
+function storeScrollPosition(element) {
+  if (!hasStorage) return;
+  localStorage.setItem(scrollPositionId(element) + "::x", $(element).scrollLeft())
+  localStorage.setItem(scrollPositionId(element) + "::y", $(element).scrollTop())
+}
+
+function recallScrollPosition(element) {
+  if (!hasStorage) return;
+  $(element).scrollLeft(localStorage.getItem(scrollPositionId(element) + "::x"))
+  $(element).scrollTop(localStorage.getItem(scrollPositionId(element) + "::y"))
+}
+
+function persistScrollPosition(element) {
+  recallScrollPosition(element)
+  $(element).scroll(function() { storeScrollPosition(element) })
+}
+
+$(window).ready(setCurrentVarLink)
+$(window).ready(function() { $('#content').scroll(setCurrentVarLink) })
+$(window).ready(function() { persistScrollPosition('#namespaces')})
