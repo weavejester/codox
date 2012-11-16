39bc875a9ab36d367d6d081315d8241651404ca1
James Reeves <jreeves@weavejester.com>
2012-11-16 11:32:17 -0500

    Released 0.6.2

--- codox.core/project.clj
+++ codox.core/project.clj
@@ -1,4 +1,4 @@
-(defproject codox/codox.core "0.6.1"
+(defproject codox/codox.core "0.6.2"
   :description "Generate documentation from Clojure source files"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/tools.namespace "0.1.3"]
--- codox.leiningen/project.clj
+++ codox.leiningen/project.clj
@@ -1,4 +1,4 @@
-(defproject codox/codox.leiningen "0.6.1"
+(defproject codox/codox.leiningen "0.6.2"
   :description "Codox Leiningen plugin"
   :eval-in-leiningen true
   :dependencies [[leinjacker "0.2.0"]])
