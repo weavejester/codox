673e6762f336bc5dfa56d6164d287916bb69777f
James Reeves <jreeves@weavejester.com>
2012-11-16 11:22:42 -0500

    Updated core dependencies

--- codox.core/project.clj
+++ codox.core/project.clj
@@ -1,5 +1,5 @@
 (defproject codox/codox.core "0.6.1"
   :description "Generate documentation from Clojure source files"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/tools.namespace "0.1.0"]
-                 [hiccup "1.0.0-beta1"]])
+                 [org.clojure/tools.namespace "0.1.3"]
+                 [hiccup "1.0.2"]])
