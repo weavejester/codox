dc0e48681f30bfb4a2d3c566041e6c0e09757edf
Anthony Grimes <i@raynes.me>
2012-12-20 04:14:20 -0600

    Upgrade lein-jacker to fix issues with latest lein.

--- codox.leiningen/project.clj
+++ codox.leiningen/project.clj
@@ -1,4 +1,4 @@
 (defproject codox/codox.leiningen "0.6.3"
   :description "Codox Leiningen plugin"
   :eval-in-leiningen true
-  :dependencies [[leinjacker "0.2.0"]])
+  :dependencies [[leinjacker "0.4.1"]])
