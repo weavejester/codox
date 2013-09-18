538d7d4cf1c54d497c9df48751c13f6e58f207c8
James Reeves <jreeves@weavejester.com>
2013-09-18 23:02:46 +0100

    Added URL and licenses to project files

--- codox.core/project.clj
+++ codox.core/project.clj
@@ -1,5 +1,8 @@
 (defproject codox/codox.core "0.6.5"
   :description "Generate documentation from Clojure source files"
+  :url "https://github.com/weavejester/codox"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/tools.namespace "0.2.4"]
                  [hiccup "1.0.4"]])
--- codox.leiningen/project.clj
+++ codox.leiningen/project.clj
@@ -1,4 +1,7 @@
 (defproject codox/codox.leiningen "0.6.5"
   :description "Codox Leiningen plugin"
+  :url "https://github.com/weavejester/codox"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
   :dependencies [[leinjacker "0.4.1"]])
