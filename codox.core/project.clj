4b6716f8d0713405fd05c3b6962afc04c5522e9e
James Reeves <jreeves@weavejester.com>
2013-09-18 23:03:00 +0100

    Revert "Updated tools.namespace version to 0.2.4"
    This reverts commit dfb749b32c8489698ea2585232aefcfbfc15fd11.
    
    Commit introduced compatibility issues with projects that used older
    versions of Clojure.

--- codox.core/project.clj
+++ codox.core/project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/tools.namespace "0.2.4"]
+                 [org.clojure/tools.namespace "0.1.3"]
                  [hiccup "1.0.4"]])
