0ffecb4ca100411fe77a452160d1d20ec14cce77
James Reeves <jreeves@weavejester.com>
2014-02-15 21:21:45 +0000

    Released 0.6.7

--- codox.leiningen/project.clj
+++ codox.leiningen/project.clj
@@ -1,4 +1,4 @@
-(defproject codox/codox.leiningen "0.6.6"
+(defproject codox/codox.leiningen "0.6.7"
   :description "Codox Leiningen plugin"
   :url "https://github.com/weavejester/codox"
   :license {:name "Eclipse Public License"
--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject codox "0.6.6"
+(defproject codox "0.6.7"
   :description "Alias for the codox/codox.leiningen plugin"
   :url "https://github.com/weavejester/codox"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[codox/codox.leiningen "0.6.6"]]
+  :dependencies [[codox/codox.leiningen "0.6.7"]]
   :eval-in-leiningen true)
