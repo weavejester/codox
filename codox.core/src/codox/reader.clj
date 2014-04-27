1082c9a91be36b3405ab5d5f85a4d93683f531bc
Richard Hull <rm_hull@yahoo.co.uk>
2014-04-27 20:16:29 +0100

    Re-org reader fn's into implementation-specific namespaces

--- codox.core/src/codox/reader.clj
+++ codox.core/src/codox/reader.clj
@@ -1,134 +1,8 @@
 (ns codox.reader
-  "Read raw documentation information from Clojure source directory."
-  (:use [codox.utils :only (unindent)])
-  (:require [clojure.java.io :as io]
-            [clojure.string :as str]
-            [clojure.tools.namespace :as ns]
-            [cljs.analyzer :as an])
-  (:import java.util.jar.JarFile))
-
-(defn- correct-indent [text]
-  (if text
-    (let [lines (str/split-lines text)]
-      (->> (rest lines)
-           (str/join "\n")
-           (unindent)
-           (str (first lines) "\n")))))
-
-(defn- sorted-public-vars [namespace]
-  (->> (ns-publics namespace)
-       (vals)
-       (sort-by (comp :name meta))))
-
-(defn- skip-public? [var]
-  (let [{:keys [skip-wiki no-doc]} (meta var)]
-    (or skip-wiki no-doc)))
-
-(defn- read-clj-publics [namespace]
-  (for [var (sorted-public-vars namespace)
-        :when (not (skip-public? var))]
-    (-> (meta var)
-        (select-keys
-         [:name :file :line :arglists :doc :macro :added :deprecated])
-        (update-in [:doc] correct-indent))))
-
-(defn- read-clj-ns [namespace]
-  (try
-    (require namespace)
-    (-> (find-ns namespace)
-        (meta)
-        (assoc :name namespace)
-        (assoc :publics (read-clj-publics namespace))
-        (update-in [:doc] correct-indent)
-        (list))
-    (catch Exception e
-      (println (format "Could not generate clojure documentation for %s - root cause: %s %s"
-                       namespace
-                       (.getName (class e))
-                       (.getMessage e))))))
-
-(defn- jar-file? [file]
-  (and (.isFile file)
-       (-> file .getName (.endsWith ".jar"))))
-
-(defn- find-clj-namespaces [file]
-  (cond
-   (.isDirectory file) (ns/find-namespaces-in-dir file)
-   (jar-file? file)    (ns/find-namespaces-in-jarfile (JarFile. file))))
-
-(defn read-clj-namespaces
-  ([]
-     (read-clj-namespaces "src"))
-  ([path]
-     (->> (io/file path)
-          (find-clj-namespaces)
-          (mapcat read-clj-ns)))
-  ([path & paths]
-     (mapcat read-clj-namespaces (cons path paths))))
-
-(defn- cljs-file? [file]
-  (and (.isFile file)
-       (-> file .getName (.endsWith ".cljs"))))
-
-(defn strip-parent [parent]
-  (let [len (inc (count (.getPath parent)))]
-    (fn [child]
-      (let [child-name (.getPath child)]
-        (when (>= (count child-name) len)
-          (io/file (subs child-name len)))))))
-
-(defn find-cljs-files [file]
-  (when (.isDirectory file)
-    (keep
-      (strip-parent file)
-      (filter
-        cljs-file?
-        (file-seq file)))))
-
-(defn read-cljs-publics [analysis namespace file]
-  (sort-by
-   :name
-    (for [[name opts] (get-in analysis [:cljs.analyzer/namespaces namespace :defs])]
-      (->
-        opts
-        (select-keys [:line :doc :macro :added :deprecated])
-        (update-in [:doc] correct-indent)
-        (assoc :file (.getPath file)
-               :arglists (second (:arglists opts)) ; dont know why cljs.analyzer double quotes this..
-               :name name)))))
-
-(defn read-cljs-file [path file]
-  (try
-    (let [analysis (an/analyze-file (io/file path file))]
-      (apply merge
-        (for [namespace (keys (:cljs.analyzer/namespaces analysis))
-              :let [doc (get-in analysis [:cljs.analyzer/namespaces namespace :doc])]]
-          {namespace
-            {:name namespace
-             :publics (read-cljs-publics analysis namespace file)
-             :doc (correct-indent doc)}})))
-    (catch Exception e
-      (println
-        (format
-          "Could not generate clojurescript documentation for %s - root cause: %s %s"
-          file
-          (.getName (class e))
-          (.getMessage e))))))
-
-(defn read-cljs-namespaces
-  ([]
-     (read-cljs-namespaces "src"))
-  ([path]
-     (->>
-       (find-cljs-files (io/file path))
-       (map (partial read-cljs-file (io/file path)))
-       (apply merge)
-       (vals)
-       (sort-by :name)))
-  ([path & paths]
-     (->>
-       (cons path paths)
-       (mapcat read-cljs-namespaces))))
+  "Read raw documentation information from source directories."
+  (:require
+    [codox.reader.clojure :as clj]
+    [codox.reader.clojurescript :as cljs]))
 
 (defn read-namespaces
   "Read namespaces from a source directory (defaults to \"src\"), and
@@ -148,5 +22,5 @@
       :added    - the library version the var was added in"
   [& paths]
   (concat
-    (apply read-clj-namespaces paths)
-    (apply read-cljs-namespaces paths)))
+    (apply clj/read-namespaces paths)
+    (apply cljs/read-namespaces paths)))
---
+++ codox.core/src/codox/reader/clojurescript.clj
@@ -0,0 +1,73 @@
+(ns codox.reader.clojurescript
+  "Read raw documentation information from ClojureScript source directory."
+  (:use
+    [codox.utils :only [correct-indent]])
+  (:require
+    [clojure.java.io :as io]
+    [cljs.analyzer :as an]))
+
+(defn- cljs-file? [file]
+  (and
+    (.isFile file)
+    (-> file .getName (.endsWith ".cljs"))))
+
+(defn- strip-parent [parent]
+  (let [len (inc (count (.getPath parent)))]
+    (fn [child]
+      (let [child-name (.getPath child)]
+        (if (>= (count child-name) len)
+          (io/file (subs child-name len)))))))
+
+(defn- find-files [file]
+  (if (.isDirectory file)
+    (keep
+      (strip-parent file)
+      (filter
+        cljs-file?
+        (file-seq file)))))
+
+(defn- read-publics [analysis namespace file]
+  (sort-by
+   :name
+    (for [[name opts] (get-in analysis [:cljs.analyzer/namespaces namespace :defs])]
+      (->
+        opts
+        (select-keys [:line :doc :macro :added :deprecated])
+        (update-in [:doc] correct-indent)
+        (assoc
+          :file (.getPath file)
+          :arglists (second (:arglists opts)) ; dont know why cljs.analyzer double quotes this..
+          :name name)))))
+
+(defn- read-file [path file]
+  (try
+    (let [analysis (an/analyze-file (io/file path file))]
+      (apply merge
+        (for [namespace (keys (:cljs.analyzer/namespaces analysis))
+              :let [doc (get-in analysis [:cljs.analyzer/namespaces namespace :doc])]]
+          {namespace
+            {:name namespace
+             :publics (read-publics analysis namespace file)
+             :doc (correct-indent doc)}})))
+    (catch Exception e
+      (println
+        (format
+          "Could not generate clojurescript documentation for %s - root cause: %s %s"
+          file
+          (.getName (class e))
+          (.getMessage e))))))
+
+(defn read-namespaces
+  ([]
+     (read-namespaces "src"))
+  ([path]
+    (let [path (io/file path)
+          file-reader (partial read-file path)]
+      (->>
+        (find-files path)
+        (map file-reader)
+        (apply merge)
+        (vals)
+        (sort-by :name))))
+  ([path & paths]
+    (mapcat read-namespaces (cons path paths))))
