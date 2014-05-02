2361b9821d2266e93481705434b4988fb1b1a14e
James Reeves <jreeves@weavejester.com>
2014-05-02 23:03:59 +0100

    Added cljs example file with same name as clj file

---
+++ codox.example/src/codox/example.cljs
@@ -0,0 +1,10 @@
+(ns codox.example
+  "ClojureScript version.")
+
+(defn foo
+  "This is a function in Clojure and ClojureScript."
+  [x y & z])
+
+(defn quzquz
+  "This is a ClojureScript-only function."
+  [x])
