1ce942b7c322d4dacee39514543f7a43d1458ded
James Reeves <jreeves@weavejester.com>
2014-05-03 13:28:01 +0100

    Made Clojure/ClojureScript choice explicit

--- codox.core/src/codox/main.clj
+++ codox.core/src/codox/main.clj
@@ -1,7 +1,8 @@
 (ns codox.main
   "Main namespace for generating documentation"
-  (:use [codox.utils :only (ns-filter add-source-paths)]
-        [codox.reader :only (read-namespaces)]))
+  (:use [codox.utils :only (ns-filter add-source-paths)])
+  (:require [codox.reader.clojure :as clj]
+            [codox.reader.clojurescript :as cljs]))
 
 (defn- writer [{:keys [writer]}]
   (let [writer-sym (or writer 'codox.writer.html/write-docs)
@@ -16,13 +17,21 @@
       (throw
          (Exception. (str "Could not resolve codox writer " writer-sym))))))
 
+(def namespace-readers
+  {:clojure       clj/read-namespaces
+   :clojurescript cljs/read-namespaces})
+
 (defn generate-docs
   "Generate documentation from source files."
   ([]
      (generate-docs {}))
-  ([{:keys [sources include exclude] :as options}]
-     (let [namespaces (-> (apply read-namespaces sources)
+  ([{:keys [language sources include exclude]
+     :or   {language :clojure}
+     :as   options}]
+     (let [write-fn   (writer options)
+           namespaces (-> (namespace-readers language)
+                          (apply sources)
                           (ns-filter include exclude)
-                          (add-source-paths sources))
-           write (writer options)]
-       (write (assoc options :namespaces namespaces)))))
+                          (add-source-paths sources))]
+       (write-fn
+        (assoc options :namespaces namespaces)))))
