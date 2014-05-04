5148e4cc8d278caace3b827f4c898963da0c99d1
James Reeves <jreeves@weavejester.com>
2014-05-04 01:03:07 +0100

    Updated tools.namespace to 0.2.4

--- codox.core/project.clj
+++ codox.core/project.clj
@@ -5,6 +5,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.namespace "0.1.3"]
+                 [org.clojure/tools.namespace "0.2.4"]
                  [org.clojure/clojurescript "0.0-2202"]
                  [hiccup "1.0.5"]])
