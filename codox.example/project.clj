6e0af169afceffd9f470d5037e9d6f2014be2989
James Reeves <jreeves@weavejester.com>
2014-05-15 23:59:07 +0100

    Removed extra dependency from codox.example

--- codox.example/project.clj
+++ codox.example/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"] [me.raynes/cegdown "0.1.1"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]]
   :plugins [[codox "0.8.1"]]
   :source-paths ["src/clojure"]
   :codox
