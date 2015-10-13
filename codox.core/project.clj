5dc94c0cccf8ecb7ba6ac0bacacbaf8ba5097597
James Reeves <jreeves@weavejester.com>
2015-10-13 05:41:36 +0100

    Fix broken core dependencies

--- codox.core/project.clj
+++ codox.core/project.clj
@@ -5,7 +5,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/tools.namespace "0.3.0"]
-                 [org.clojure/clojurescript "1.7.112"]
+                 [org.clojure/tools.namespace "0.2.11"]
+                 [org.clojure/clojurescript "1.7.122"]
                  [hiccup "1.0.5"]
                  [org.pegdown/pegdown "1.6.0"]])
