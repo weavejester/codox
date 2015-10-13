d256ec479bb0bf5ec59ee586e9e2cb16eb3c8c58
James Reeves <jreeves@weavejester.com>
2015-10-13 22:38:04 +0100

    Add explicit asm-all dependency to codox.core
    This is a workaround for using Codox with core.async, which uses an
    older version of the ASM library. Fixes #101 in most cases.

--- codox.core/project.clj
+++ codox.core/project.clj
@@ -8,4 +8,5 @@
                  [org.clojure/tools.namespace "0.2.11"]
                  [org.clojure/clojurescript "1.7.122"]
                  [hiccup "1.0.5"]
-                 [org.pegdown/pegdown "1.6.0"]])
+                 [org.pegdown/pegdown "1.6.0"]
+                 [org.ow2.asm/asm-all "5.0.3"]])
