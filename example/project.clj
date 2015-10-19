8a58512c4b3fd151b61c716d5b518ce098ed15d3
James Reeves <jreeves@weavejester.com>
2015-10-19 22:43:24 +0100

    Update ClojureScript version

--- example/project.clj
+++ example/project.clj
@@ -16,7 +16,7 @@
   :profiles
   {:md   {:codox {:metadata {:doc/format :markdown}}}
    :cljs {:dependencies [[org.clojure/clojure "1.7.0"]
-                         [org.clojure/clojurescript "1.7.122"]]
+                         [org.clojure/clojurescript "1.7.145"]]
           :codox {:language :clojurescript}}
    :om   {:dependencies [[org.omcljs/om "0.8.8"]]}
    :no-src {:codox ^:replace {}}
