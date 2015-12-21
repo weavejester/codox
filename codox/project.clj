ca09fa4e93d214fb06b87bb3929f09bbaa8dfe20
James Reeves <jreeves@weavejester.com>
2015-12-21 22:21:00 +0000

    Update ClojureScript version

--- codox/project.clj
+++ codox/project.clj
@@ -6,7 +6,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.namespace "0.2.11"]
-                 [org.clojure/clojurescript "1.7.145"]
+                 [org.clojure/clojurescript "1.7.189"]
                  [hiccup "1.0.5"]
                  [org.pegdown/pegdown "1.6.0"]
                  [org.ow2.asm/asm-all "5.0.3"]])
